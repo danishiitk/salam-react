c89e16e5d929fed423a7bf5561b0b45207fe1548
Danish Ahmad <danish.ahmad@oracle.com>
2025-03-27 17:02:34 +0530

    Finished react starting project

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const reactTags = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1"),
-    React.createElement("h2", {}, "I am h2"),
+  React.createElement("div", { id: "child1", key: "parent-key1" }, [
+    React.createElement("h1", { key: "key1" }, "I am h1"),
+    React.createElement("h2", { key: "key2" }, "I am h2"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1"),
-    React.createElement("h2", {}, "I am h2"),
+  React.createElement("div", { id: "child2", key: "parent-key2" }, [
+    React.createElement("h1", { key: "key3" }, "I am h1"),
+    React.createElement("h2", { key: "key4" }, "I am h2"),
   ]),
 ]);
 const rootReact = ReactDOM.createRoot(document.getElementById("root"));
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 #Salam react
+1 - Basic index html, index css, App.js setup
+2 - Remote: Setup github > create repo
+Local: git init, git add, git commit, git push setup/copy remote url from github and setup
+
+3 - npm init --> this will generate package.json, package-lock.json
+4 - .gitignore --> Add /node_modules etc for git to ignore them
+
+5 - We choose parcel as dependency manager,
+so install it as: npm install -D parcel
+install react and react-dom: npm i react and npm i react-dom
+
+6 - To run the app: npx parcel index.html
